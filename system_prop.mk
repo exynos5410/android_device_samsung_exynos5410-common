34d7b985a8532362c90af8d2c8aea773bd3305b3
Alberto Guanti <albertoguanti@gmail.com>
2017-07-17 21:31:49 +0200

    exynos5410-common: prop: Kang some values from hardware_ti_omap4 repo
    * TESTING

--- system_prop.mk
+++ system_prop.mk
@@ -12,6 +12,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.render_dirty_regions=false \
+    debug.hwui.swap_with_damage=false \
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    ro.egl.destroy_after_detach=true \
+    ro.hwui.disable_scissor_opt=true \
     ro.opengles.version=131072 \
     ro.sf.lcd_density=480 \
     ro.zygote.disable_gl_preload=1
