926f0b740c20d98a7d5565b811a5ff5595bdb2b7
Alberto Guanti <albertoguanti@gmail.com>
2017-10-18 23:45:42 +0200

    exynos5410: enable variable touchkey brightness support
    * based on exynos5420 implementation

--- include/samsung_lights.h
+++ include/samsung_lights.h
@@ -30,6 +30,9 @@
 #define BUTTON_BRIGHTNESS_NODE "/sys/class/sec/sec_touchkey/brightness"
 #define LED_BLINK_NODE "/sys/class/sec/led/led_blink"
 
+// Variable button brightness
+#define VAR_BUTTON_BRIGHTNESS 1
+
 /*
  * Brightness adjustment factors
  *
